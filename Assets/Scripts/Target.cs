d09ce4d5dc54d9f7b7a52cfd32f514ecbdc92731
liyiming-serif <liyiming.serif@gmail.com>
2016-08-17 23:56:40 -0400

    added boundaries + code for target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class Target : MonoBehaviour {
+	[SerializeField]
+	int value; //points player gets for hitting this target
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +13,13 @@ public class Target : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Projectile") {
+			Engine.score += value;
+
+		}
 	}
 }
