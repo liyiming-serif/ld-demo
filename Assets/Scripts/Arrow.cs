78750db3ec6fb90877477a2a064d906e118e4826
liyiming-serif <liyiming.serif@gmail.com>
2016-08-17 22:38:16 -0400

    changed arrow.cs

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -2,28 +2,28 @@
 using System.Collections;
 
 public class Arrow : MonoBehaviour {
-	//Rigidbody2D rigidbody;
+	Rigidbody2D body;
 	Vector2 v; //velocity once rigidbody is attached
 	//flags to track the state of the arrow
-	bool notched;
+	//bool notched;
 	bool stuck;
 
 	// Use this for initialization
 	void Start() {
-		notched = true;
+		//notched = true;
 		stuck = false;
-		Debug.Log(Engine.score);
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		//listen for when arrow has been fired
+		/*listen for when arrow has been fired
 		if(gameObject.GetComponent<Rigidbody2D>()!=null && notched==true) {
 			notched = false;
 			StartCoroutine(TrackAngle());
-		}
+		}*/
 	}
 
+	//control arrow's image based on trajectory. stops when arrow sticks.
 	IEnumerator TrackAngle() {
 		while(stuck == false) {
 			v = gameObject.GetComponent<Rigidbody2D>().velocity;
@@ -33,6 +33,14 @@ public class Arrow : MonoBehaviour {
 		}
 	}
 
+	//arrow is fired from bow
+	public void FireArrow(Vector2 initVelocity) {
+		body = gameObject.AddComponent<Rigidbody2D>();
+		body.AddForce(initVelocity, ForceMode2D.Impulse);
+		StartCoroutine(TrackAngle());
+	}
+
+	//runs when arrow hits barrier
 	void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.tag == "Barrier") {
 			stuck = true;
