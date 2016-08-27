a2db0e8079c6fdbd07a89a001c3bf55e336b124a
alexwzk <alexanderwzk@gmail.com>
2016-08-27 13:57:43 -0400

    fix naming issues

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -1,11 +1,8 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 public class Bow : MonoBehaviour {
 
-    public Arrow arrow;
-
-    
     // to determine the mouse position, we need a raycast
     Ray mouseRay;
     RaycastHit rayHit;
@@ -14,21 +11,6 @@ public class Bow : MonoBehaviour {
     float posX;
     float posY;
 
-    // to determine the string pullout
-    float arrowStartX;
-    float length;
-
-    // dynamically render the string line
-    public GameObject bowString;
-    
-    // the bowstring is a line renderer
-    List<Vector3> bowStringPosition;
-    LineRenderer bowStringLinerenderer;
-
-    // position of the line renderers middle part 
-    Vector3 stringPullout;
-    Vector3 stringRestPosition = new Vector3(-0.44f, -0.06f, 2f);
-
     //
     // void Start()
     //
@@ -36,26 +18,7 @@ public class Bow : MonoBehaviour {
     //
     void Start()
     {
-        Engine.singleton.arrowShot = false;
-        Engine.singleton.arrowPrepared = false;
 
-        // setup the line renderer representing the bowstring
-        bowStringLinerenderer = bowString.AddComponent<LineRenderer>();
-        bowStringLinerenderer.SetVertexCount(3);
-        bowStringLinerenderer.SetWidth(0.05F, 0.05F);
-        bowStringLinerenderer.useWorldSpace = false;
-        bowStringLinerenderer.material = Resources.Load("Materials/bowStringMaterial") as Material;
-        bowStringPosition = new List<Vector3>();
-        bowStringPosition.Add(new Vector3(-0.35f, 1.0f, 2f));
-        bowStringPosition.Add(new Vector3(-0.34f, -0.06f, 2f));
-        bowStringPosition.Add(new Vector3(-0.35f, -1.0f, 2f));
-        bowStringLinerenderer.SetPosition(0, bowStringPosition[0]);
-        bowStringLinerenderer.SetPosition(1, bowStringPosition[1]);
-        bowStringLinerenderer.SetPosition(2, bowStringPosition[2]);
-        bowStringLinerenderer.sortingLayerName = "Animator";
-        bowStringLinerenderer.sortingOrder = 10;
-        arrowStartX = 0.7f;
-        stringPullout = stringRestPosition;
     }
 
     //
@@ -65,22 +28,22 @@ public class Bow : MonoBehaviour {
     //
     public void SpawnArrow()
     {
-        //TODO sound effect
-        // instantiate a new arrow
-        transform.localRotation = Quaternion.identity;
-        Debug.Log("here");
-        arrow = (Instantiate(Resources.Load("arrowPrefab"), Vector3.zero, Quaternion.identity) as GameObject).GetComponent<Arrow>();
-        if(arrow == null)
-        {
-            Debug.Log("Instantiation failed.");
-        }
-        arrow.transform.name = "arrow";
-        arrow.transform.localScale = transform.localScale;
-        arrow.transform.localPosition = transform.position;
-        arrow.transform.localRotation = transform.localRotation;
-        arrow.transform.parent = transform;
-        Engine.singleton.arrowShot = false;
-        Engine.singleton.arrowPrepared = false;
+        ////TODO sound effect
+        //// instantiate a new arrow
+        //transform.localRotation = Quaternion.identity;
+        //Debug.Log("here");
+        //arrow = (Instantiate(Resources.Load("arrowPrefab"), Vector3.zero, Quaternion.identity) as GameObject).GetComponent<Arrow>();
+        //if(arrow == null)
+        //{
+        //    Debug.Log("Instantiation failed.");
+        //}
+        //arrow.transform.name = "arrow";
+        //arrow.transform.localScale = transform.localScale;
+        //arrow.transform.localPosition = transform.position;
+        //arrow.transform.localRotation = transform.localRotation;
+        //arrow.transform.parent = transform;
+        //Engine.singleton.arrowShot = false;
+        //Engine.singleton.arrowPrepared = false;
     }
 
     // public void PullString()
@@ -89,28 +52,28 @@ public class Bow : MonoBehaviour {
     //
     public void PullString()
     {
-        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(mouseRay, out rayHit, 1000f) && Engine.singleton.arrowShot == false)
-        {
-            //TODO max turning radius should not be over +\-90.
-            // determine the position on the screen
-            posX = rayHit.point.x;
-            posY = rayHit.point.y;
-            // set the bows angle to the arrow
-            Vector2 mousePos = new Vector2(transform.position.x - posX, transform.position.y - posY);
-            float angleZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, 0, angleZ);
-            // determine the arrow pullout
-            length = mousePos.magnitude / 3f;
-            length = Mathf.Clamp(length, 0, 1);
-            // set the bowstrings line renderer
-            stringPullout = new Vector3(-(length - 0.2f), -0.06f, 2f);
-            // set the arrows position
-            Vector3 arrowPosition = arrow.transform.localPosition;
-            arrowPosition.x = (arrowStartX - length);
-            arrow.transform.localPosition = arrowPosition;
-        }
-        Engine.singleton.arrowPrepared = true;
+        //mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(mouseRay, out rayHit, 1000f) && Engine.singleton.arrowShot == false)
+        //{
+        //    //TODO max turning radius should not be over +\-90.
+        //    // determine the position on the screen
+        //    posX = rayHit.point.x;
+        //    posY = rayHit.point.y;
+        //    // set the bows angle to the arrow
+        //    Vector2 mousePos = new Vector2(transform.position.x - posX, transform.position.y - posY);
+        //    float angleZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        //    transform.eulerAngles = new Vector3(0, 0, angleZ);
+        //    // determine the arrow pullout
+        //    length = mousePos.magnitude / 3f;
+        //    length = Mathf.Clamp(length, 0, 1);
+        //    // set the bowstrings line renderer
+        //    stringPullout = new Vector3(-(length - 0.2f), -0.06f, 2f);
+        //    // set the arrows position
+        //    Vector3 arrowPosition = arrow.transform.localPosition;
+        //    arrowPosition.x = (arrowStartX - length);
+        //    arrow.transform.localPosition = arrowPosition;
+        //}
+        //Engine.singleton.arrowPrepared = true;
     }
 
     //
@@ -121,23 +84,10 @@ public class Bow : MonoBehaviour {
     //
     public void FireArrow()
     {
-        arrow.transform.parent = Engine.singleton.transform;
-        arrow.FireArrow(Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z)) * new Vector2(25f * length, 0));
-        Engine.singleton.arrowPrepared = false;
-        stringPullout = stringRestPosition;
+        //arrow.transform.parent = Engine.singleton.transform;
+        //arrow.FireArrow(Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z)) * new Vector2(25f * length, 0));
+        //Engine.singleton.arrowPrepared = false;
+        //stringPullout = stringRestPosition;
     }
 
-    //
-    // public void DrawString()
-    //
-    // set the bowstrings line renderer position
-    //
-    public void DrawString()
-    {
-        bowStringLinerenderer = bowString.GetComponent<LineRenderer>();
-        bowStringLinerenderer.SetPosition(0, bowStringPosition[0]);
-        bowStringLinerenderer.SetPosition(1, stringPullout);
-        bowStringLinerenderer.SetPosition(2, bowStringPosition[2]);
-    }
 }
-*/
