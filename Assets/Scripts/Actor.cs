13c72975de2c3fa742935ffa461cc13e76f95b03
liyiming-serif <liyiming.serif@gmail.com>
2016-08-27 13:56:50 -0400

    fixed animation. jump is now w/up key

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -6,25 +6,24 @@ using System.Collections;
  * Can walk, jump, and hop on sloped surfaces.
  * Can be extended to flying and crawling characters.
  * REQUIRES gameObject to have a GroundCheck as its child.
+ * Every Actor MUST agree on this Animator fsm schema.
  */
 [RequireComponent(typeof (Rigidbody2D))]
 
 public class Actor : MonoBehaviour {
+
+	public float jumpHeight;
+	[Range(0,1)] public float hopHeight;
+	public float moveSpeed;
+	public Vector2 cameraOffset; //a buffer for the following camera.
 
 	//helper members
 	bool onGround;
 	Rigidbody2D body;
 	public enum Face{Left, Right};
 	[HideInInspector] public Face dir;
-	Animator animation;
-
-	//grants spatial awareness
+	Animator animate;
 	Transform groundCheck; //check if this actor is on ground
-
-	public float jumpHeight;
-	[Range(0,1)] public float hopHeight;
-	public float moveSpeed;
-	public Vector2 cameraOffset; //a buffer for the following camera.
 
 	// Use this for initialization
 	void Awake () {
@@ -32,48 +31,60 @@ public class Actor : MonoBehaviour {
 		groundCheck = transform.Find("GroundCheck");
 		onGround = false;
 		dir = Face.Right;
-		animation = GetComponent<Animator>();
+		animate = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Solid"));
-		if(onGround)
-			animation.SetTrigger("land");
-		else if(body.velocity.y < 0)
-			animation.SetTrigger("fall");
+
+		//ANIMATOR: handle everything in the air
+		if(!onGround) {
+			animate.SetBool("onGround", false);
+			if(body.velocity.y > 0)
+				animate.SetBool("falling", false);
+			else
+				animate.SetBool("falling", true);
+		}
+		else
+			animate.SetBool("onGround", true);
 	}
 
 	/**Called every FixedUpdate of the controller
 	 */
 	public void Move(float xTilt) { //NOTE: xTilt must be calculated for enemy AI.
 		body.velocity = new Vector2(xTilt * moveSpeed, body.velocity.y);
+
 		if(xTilt != 0) {
-			animation.SetTrigger("startWalk");
-		} else {
-			animation.SetTrigger("stopWalk");
+			//ANIMATOR: start/maintain walk cycle if on ground
+			if(onGround) {
+				animate.SetTrigger("startWalk");
+				animate.speed = Mathf.Abs(xTilt);
+			}
+
+			//Turn Around
+			if(Mathf.Sign(xTilt) != Mathf.Sign(transform.localScale.x)) {
+				Vector3 temp = transform.localScale;
+				temp.x *= -1;
+				transform.localScale = temp;
+
+				cameraOffset.x *= -1;
+				Engine.singleton.ChangeCameraOffset(gameObject, cameraOffset);
+
+				if(temp.x > 0)
+					dir = Face.Right;
+				else if(temp.x < 0)
+					dir = Face.Left;
+			}
 		}
-
-		//Turn Around
-		if(Mathf.Sign(xTilt) != Mathf.Sign(transform.localScale.x) && xTilt != 0) {
-			Vector3 temp = transform.localScale;
-			temp.x *= -1;
-			transform.localScale = temp;
-
-			cameraOffset.x *= -1;
-			Engine.singleton.ChangeCameraOffset(gameObject, cameraOffset);
-
-			if(temp.x > 0)
-				dir = Face.Right;
-			else if(temp.x < 0)
-				dir = Face.Left;
+		else {
+			animate.SetTrigger("stopWalk");
 		}
 	}
 
 	public void Jump() {
 		if(onGround) {
 			body.velocity = new Vector2(body.velocity.x, jumpHeight);
-			animation.SetTrigger("startJump");
 		}
 	}
 
