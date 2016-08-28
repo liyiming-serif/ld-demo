c3a0ba686d3187b427140737f7d304493ae5299d
liyiming-serif <liyiming.serif@gmail.com>
2016-08-27 23:51:56 -0400

    implemented base class target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,28 +3,40 @@ using System.Collections;
 
 public class Target : MonoBehaviour {
 
-	[SerializeField] private CircleCollider2D innerSight;
-	[SerializeField] private CircleCollider2D outerSight;
+	[SerializeField] private float innerSight = 80f;
+	[SerializeField] private float outerSight = 200f;
+	private CircleCollider2D sightTrigger;
 
 	void Start()
 	{
-		
+		sightTrigger = gameObject.AddComponent<CircleCollider2D>() as CircleCollider2D;
+		sightTrigger.radius = outerSight;
+		sightTrigger.isTrigger = true;
 	}
 
 	//death sequence when hit by arrow
-	protected virtual void OnCollisionEnter2D(Collision2D other)
+	protected virtual void OnColliderEnter2D(Collision2D other)
 	{
 		if(other.gameObject.tag == "Projectile") {
 			Die();
 		}
-		else if(other.gameObject.tag == "Player") {
+	}
+
+	protected virtual void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player") {
 			Panic();
 		}
 	}
 
+	public void Alert(Vector2 landingSpot) {
+		float proximity = Vector2.Distance(landingSpot, transform.position);
+		if(proximity <= innerSight)
+			Panic();
+	}
+
 	protected virtual void Die(){
 		Destroy(gameObject);
-		Debug.Log("!");
 	}
 
 	protected virtual void Panic() {
